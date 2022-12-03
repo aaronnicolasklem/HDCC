9b3e93c48bfa7e1e170db1875160107108dc92d1
Aaron Klem <aaronnicolasklem@gmail.com>
2022-12-03 15:43:37 -0500

    Add svg path w/ color change. Working on d3 hookup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const hexbin = d3.hexbin();
+//const hexbin = d3.hexbin();
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
