c391353222d83fc19505080a4ee40ac37423e325
Aaron Klem <aaronnicolasklem@gmail.com>
2022-12-05 20:25:25 -0500

    Quick edit to not miss 2 days

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const hexbin = d3.hexbin();
+const http = require('http');
+var express = require('express');
+var app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage');
+  });
+  
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
