b769d650b05715f5a9c5ddc056ae1c15c01bddf7
Aaron Klem <aaronnicolasklem@gmail.com>
2022-12-10 14:30:43 -0500

    Messed with server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 //const hexbin = d3.hexbin();
 
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
 
+
+/*const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(`...`);
+*/
 app.get('/', (req, res) => {
   res.send('index.html');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
